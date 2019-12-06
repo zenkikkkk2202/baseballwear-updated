2c8ebbfe3beb2753bc273ec0049d7c884c6f3848
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2019-12-06 17:34:09 +0900

    フロント実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+  devise_for :users
+  root to: "posts#index"
+  resources :users, only: [:edit, :update]
 end
