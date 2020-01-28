7c428ce15067bcc88d1aa3f60e518efeacc810b0
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-28 13:18:41 +0900

    asynchronous_good_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,5 @@
 Rails.application.routes.draw do 
   devise_for :users
-  # , controllers: {
-  #   registrations: 'users/registrations'
-  # }
-  # devise_scope :user do
-  #   get 'users/thanks' => 'users/registrations#thanks'
-  # end
-
-  # def devise_scope(scope)
-  #   constraint = lambda do |request|
-  #     request.env["devise.mapping"] = Devise.mappings[scope]
-  #     true
-  #   end
-
-  #   constraints(constraint) do
-  #     yield
-  #   end
-  # end
 
   root to: "posts#top"
   resources :posts do 
