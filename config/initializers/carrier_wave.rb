53b9bfa2d69625e4d0a5d794b548710d410c761d
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-03-12 11:43:42 +0900

    change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,13 +3,13 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'], 
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
+    config.fog_directory     =  'baseballwear2'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/baseballwear2'
   end
 
   # 日本語ファイル名の設定
