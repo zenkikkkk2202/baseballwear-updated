785e7999fcaa9d558c6f6cc0996ad2fff0c386d0
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-03-12 12:22:48 +0900

    change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,17 +1,45 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.storage = :fog
+#     config.fog_provider = 'fog/aws' 
+#     config.fog_credentials = {
+#       # Amazon S3用の設定
+#       provider: 'AWS',
+#       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+#       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+#       region: 'ap-northeast-1'
+#     }
+#     config.fog_directory     =  'baseballwear2'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/baseballwear2'
+#   end
+
+#   # 日本語ファイル名の設定
+#   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
+# end
+
+
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws' 
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials[:secret_access_key],
-      region: 'ap-northeast-1'
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
     }
-    config.fog_directory     =  'baseballwear2'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/baseballwear2'
+    config.fog_directory     =  ENV['AWS_S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 
   # 日本語ファイル名の設定
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
