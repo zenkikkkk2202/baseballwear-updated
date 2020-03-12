6772aaf3678c6a6cab241e6584cd18bd2a66ef6a
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-03-12 16:47:18 +0900

    change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -33,10 +33,10 @@ if Rails.env.production?
       # Amazon S3用の設定
       provider: 'AWS',
       region: 'ap-northeast-1',
-      # aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      # aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
     }
     config.fog_directory     =  'baseballwear2'
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
