205330b81dce4f95117cd3dabe80ec13bfc62c35
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-03-12 13:39:14 +0900

    change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
 # if Rails.env.production?
 #   CarrierWave.configure do |config|
@@ -33,8 +33,10 @@ if Rails.env.production?
       # Amazon S3用の設定
       provider: 'AWS',
       region: ENV['AWS_S3_REGION'],
-      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      # aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      # aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
     }
     config.fog_directory     =  ENV['AWS_S3_BUCKET']
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
