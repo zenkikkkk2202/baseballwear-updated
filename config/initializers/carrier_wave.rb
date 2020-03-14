7565f1385302f61f8b850abeeda6757846918898
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-03-15 00:50:31 +0900

    change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+    aws_access_key_id: ENV['ACCESS_KEY'],
+    aws_secret_access_key: ENV['SECRET_KEY'],
     region: 'ap-northeast-1'
   }
 
